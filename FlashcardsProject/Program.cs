679001a6d362f780566fa854d65c3306c393b558
xpolaczeqx@gmail.com <xpolaczeqx@gmail.com>
2026-08-11 22:37:19 +0200

    added empty constructor for flashcard class

--- FlashcardsProject/Program.cs
+++ FlashcardsProject/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System; 
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace FlashCardsProject
 {
@@ -6,14 +9,29 @@ namespace FlashCardsProject
 	{
 		static void Main(string[] args)
 		{
+			List<Flashcard> flashcards = new List<Flashcard>();
+
+			if (File.Exists("flashcards.json"))
+			{
+				string ImportedFlashcards =  JsonSerializer.Deserialize<List<Flashcard>>(File.ReadAllText("flashcards.json"));
+			}
+			else
+			{
+				
+			}
 			
 		}
 	}
 
 	class Flashcard
 	{
+		
 		public string question {get; set; }
 		public string answer {get; set; }
+
+		public Flashcard()
+		{
+		}
 
 		public Flashcard(string question, string answer)
 		{this.question = question ;this.answer = answer;}
