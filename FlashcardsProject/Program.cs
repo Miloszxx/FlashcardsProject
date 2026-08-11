7a8527d1982a835fea572b21d0ecf963eb8de343
xpolaczeqx@gmail.com <xpolaczeqx@gmail.com>
2026-08-11 22:47:04 +0200

    add flashcard creation and json serialization

--- FlashcardsProject/Program.cs
+++ FlashcardsProject/Program.cs
@@ -13,13 +13,33 @@ namespace FlashCardsProject
 
 			if (File.Exists("flashcards.json"))
 			{
-				string ImportedFlashcards =  JsonSerializer.Deserialize<List<Flashcard>>(File.ReadAllText("flashcards.json"));
+				string ImportedText = File.ReadAllText("flashcards.json");
+				flashcards = JsonSerializer.Deserialize<List<Flashcard>>(ImportedText);
 			}
-			else
+
+			int choice = 0;
+
+			while (choice != 3)
 			{
-				
+				Console.WriteLine("What would you like to do?");
+				Console.WriteLine("1. Add flashcard");
+				Console.WriteLine("2. Study mode");
+				Console.WriteLine("3. Exit");
+				choice = Convert.ToInt32(Console.ReadLine());
+				switch  (choice)
+				{
+					case 1:
+						Console.WriteLine("Question:");
+						string question = Console.ReadLine();
+						Console.WriteLine("Answer:");
+						string answer = Console.ReadLine();
+						flashcards.Add(new Flashcard(question, answer));
+						string json = JsonSerializer.Serialize(flashcards);
+						File.WriteAllText("flashcards.json", json);
+						break;
+				}
 			}
-			
+
 		}
 	}
 
