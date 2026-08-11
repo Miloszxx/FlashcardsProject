19b8806572d4f5f05fe53cf07029a6d9f6692ef3
xpolaczeqx@gmail.com <xpolaczeqx@gmail.com>
2026-08-11 22:09:40 +0200

    init: added c# boilerplate start

--- FlashcardsProject/Program.cs
+++ FlashcardsProject/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System; 
 
-Console.WriteLine("Hello, World!");
+namespace FlashCardsProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
